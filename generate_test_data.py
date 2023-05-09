aa40157a826e7c573da4548ab2374915a90184b5
Hemadityamalla <hemadityamalla@gmail.com>
2023-05-09 11:48:54 +0200

    trying the 2d case for a better understanding

--- generate_test_data.py
+++ generate_test_data.py
@@ -3,12 +3,12 @@ from scipy.constants import speed_of_light
 import matplotlib.pyplot as plt
 
 # Domain dimensions and time arrays
-ndim = 3
-# Nt, Nx, Ny, Nz = 50, 50, 50, 50
-# Nt, Nx, Ny, Nz = 100, 17, 17, 17
-Nt, Nx, Ny, Nz = 50, 33, 33, 33
-# Nt, Nx, Ny, Nz = 50, 17, 17, 17
-# Nt, Nx, Ny, Nz = 50, 9, 9, 9
+ndim = 2
+# Nt, Nx, Ny = 50, 50, 50
+# Nt, Nx, Ny = 100, 17, 17
+Nt, Nx, Ny = 50, 33, 33
+# Nt, Nx, Ny = 50, 17, 17
+# Nt, Nx, Ny = 50, 9, 9
 
 tstart, tend = 0.0, 1e-6
 t = np.linspace(tstart, tend, Nt)
@@ -16,61 +16,62 @@ t = np.linspace(tstart, tend, Nt)
 lx, ly, lz = 0.2, 0.2, 0.2
 x = np.linspace(-lx/2, lx/2, Nx)
 y = np.linspace(-ly/2, ly/2, Ny)
-z = np.linspace(-lz/2, lz/2, Nz)
-xx, yy, zz = np.meshgrid(x, y, z, indexing='ij')
-dV = (x[1] - x[0]) * (y[1] - y[0]) * (z[1] - z[0])
+xx, yy = np.meshgrid(x, y, indexing='ij')
+dV = (x[1] - x[0]) * (y[1] - y[0])
 
 # Case 1
-def rho_spat(X, Y, Z, t):
+
+
+def rho_spat(X, Y, t):
     if t < 0.5*(tstart+tend):
         return np.zeros(X.shape)
     else:
         return np.ones(X.shape)
 
 
-def J_spat(X, Y, Z, t):
+def J_spat(X, Y, t):
     if t < 0.5*(tstart+tend):
-        return [np.zeros(X.shape), np.zeros(Y.shape), np.zeros(Z.shape)]
+        return [np.zeros(X.shape), np.zeros(Y.shape)]
     else:
-        return [np.ones(X.shape), np.ones(Y.shape), np.ones(Z.shape)]
+        return [np.ones(X.shape), np.ones(Y.shape)]
 
 
-rho = np.zeros((Nt, Nx, Ny, Nz))
-J = np.zeros((Nt, ndim, Nx, Ny, Nz))
+rho = np.zeros((Nt, Nx, Ny))
+J = np.zeros((Nt, ndim, Nx, Ny))
 for i, time in enumerate(t):
-    rho[i] = rho_spat(xx, yy, zz, time)
-    J[i] = J_spat(xx, yy, zz, time)
+    rho[i] = rho_spat(xx, yy, time)
+    J[i] = J_spat(xx, yy, time)
 
 print("Case 1:", rho.shape, J.shape)
-np.savez("case1.npz", t=t, rho=rho, J=J, x=x, y=y, z=z)
+np.savez("case1.npz", t=t, rho=rho, J=J, x=x, y=y)
 
 V = 0
 f = 2.0/(tend-tstart)
 d = 0.2*lz
-sigma = 0.2 * min(lx, ly, lz)
+sigma = 0.2 * min(lx, ly)
 
 
-def rho_spat(X, Y, Z, t):
+def rho_spat(X, Y, t):
     mu = V*t + d*np.sin(2*np.pi*f*t)
-    R = np.exp(- (X**2 + Y**2 + (Z-mu)**2)/(2*sigma**2))
+    R = np.exp(- (X**2 + (Y-mu)**2)/(2*sigma**2))
     denom = R.sum() * dV
     return R/denom
 
 
-def J_spat(X, Y, Z, t):
-    rho = rho_spat(X, Y, Z, t)
+def J_spat(X, Y, t):
+    rho = rho_spat(X, Y, t)
     vz = V + 2*np.pi*f*d*np.cos(2*np.pi*f*t)
-    return [np.zeros(X.shape), np.zeros(X.shape), rho*vz]
+    return [np.zeros(X.shape), rho*vz]
 
 
-rho = np.zeros((Nt, Nx, Ny, Nz))
-J = np.zeros((Nt, ndim, Nx, Ny, Nz))
+rho = np.zeros((Nt, Nx, Ny))
+J = np.zeros((Nt, ndim, Nx, Ny))
 for i, time in enumerate(t):
-    rho[i] = rho_spat(xx, yy, zz, time)
-    J[i] = J_spat(xx, yy, zz, time)
+    rho[i] = rho_spat(xx, yy, time)
+    J[i] = J_spat(xx, yy, time)
 
 print("Case 2:", rho.shape, J.shape)
-np.savez("case2.npz", t=t, rho=rho, J=J, x=x, y=y, z=z)
+np.savez("case2.npz", t=t, rho=rho, J=J, x=x, y=y)
 
 # Case 3
 V = 0 #3e-4*speed_of_light
@@ -81,25 +82,25 @@ d = 0.2*lz
 t = np.linspace(tstart, tend, Nt)
 
 
-def rho_spat(X, Y, Z, t):
+def rho_spat(X, Y, t):
     mu = V*t + d*np.sin(2*np.pi*f*t)
-    sigma = 0.1 * min(lx, ly, lz)
-    R = np.exp(- (X**2 + Y**2 + (Z-mu)**2)/(2*sigma**2))
+    sigma = 0.1 * min(lx, ly)
+    R = np.exp(- (X**2 + (Y-mu)**2)/(2*sigma**2))
     denom = R.sum() * dV
     return R/denom
 
 
-def J_spat(X, Y, Z, t):
-    rho = rho_spat(X, Y, Z, t)
+def J_spat(X, Y, t):
+    rho = rho_spat(X, Y, t)
     vz = V + 2*np.pi*f*d*np.cos(2*np.pi*f*t)
-    return [np.zeros(X.shape), np.zeros(X.shape), rho*vz]
+    return [np.zeros(X.shape), rho*vz]
 
 
-rho = np.zeros((Nt, Nx, Ny, Nz))
-J = np.zeros((Nt, ndim, Nx, Ny, Nz))
+rho = np.zeros((Nt, Nx, Ny))
+J = np.zeros((Nt, ndim, Nx, Ny))
 for i, time in enumerate(t):
-    rho[i] = rho_spat(xx, yy, zz, time)
-    J[i] = J_spat(xx, yy, zz, time)
+    rho[i] = rho_spat(xx, yy, time)
+    J[i] = J_spat(xx, yy, time)
 
 print("Case 3:", rho.shape, J.shape)
-np.savez("case3.npz", t=t, rho=rho, J=J, x=x, y=y, z=z)
+np.savez("case3.npz", t=t, rho=rho, J=J, x=x, y=y)
--- jefimenko.py
+++ jefimenko.py
@@ -11,46 +11,45 @@ p = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     description='Numerical solution to Jefimenko equations')
 p.add_argument('-npz', type=str, required=True,
-               help='''Numpy file with 4D charge density scalar rho(t,x,y,z),
-               5D current density vector J(t,dim,x,y,z), time array t and
-               grid coordinate arrays x, y, z''')
+               help='''Numpy file with 3D charge density scalar rho(t,x,y),
+               4D current density vector J(t,dim,x,y), time array t and
+               grid coordinate arrays x, y''')
 p.add_argument('-observation_points', type=float, nargs='+', required=True,
-               help='N*3 coordinates of outside observation points')
+               help='N*2 coordinates of outside observation points')
 args = p.parse_args()
 
 npz_file = np.load(args.npz)
 t = npz_file['t']
-x, y, z = npz_file['x'], npz_file['y'], npz_file['z']
+x, y = npz_file['x'], npz_file['y']
 rho_samples = npz_file['rho']
 J_samples = npz_file['J']
 # Assume constant dx
-dx = np.array([x[1] - x[0], y[1] - y[0], z[1] - z[0]])
+dx = np.array([x[1] - x[0], y[1] - y[0]])
 grid_size = rho_samples.shape[1:]
-grid_coordinates = np.meshgrid(x, y, z, indexing='ij', sparse=True)
+grid_coordinates = np.meshgrid(x, y, indexing='ij', sparse=True)
 
 rho_deriv = np.gradient(rho_samples, t, axis=0)
 J_deriv = np.gradient(J_samples, t, axis=0)
-Jx_deriv, Jy_deriv, Jz_deriv = J_deriv[:, 0], J_deriv[:, 1], J_deriv[:, 2]
+Jx_deriv, Jy_deriv = J_deriv[:, 0], J_deriv[:, 1]
 
 # RegularGridInterpolator is a bit overkill, since we do not interpolate in
 # space, but the only way to vectorize time interpolation
-drho_dt = RegularGridInterpolator((t, x, y, z), rho_deriv, bounds_error=False)
-dJx_dt = RegularGridInterpolator((t, x, y, z), Jx_deriv, bounds_error=False)
-dJy_dt = RegularGridInterpolator((t, x, y, z), Jy_deriv, bounds_error=False)
-dJz_dt = RegularGridInterpolator((t, x, y, z), Jz_deriv, bounds_error=False)
+drho_dt = RegularGridInterpolator((t, x, y), rho_deriv, bounds_error=False)
+dJx_dt = RegularGridInterpolator((t, x, y), Jx_deriv, bounds_error=False)
+dJy_dt = RegularGridInterpolator((t, x, y), Jy_deriv, bounds_error=False)
 
 dV = np.product(dx)
-r_obs = np.reshape(args.observation_points, [-1, 3])
+r_obs = np.reshape(args.observation_points, [-1, 2])
 n_obs_points = len(r_obs)
 
 # Compute delays per grid point for each observation point
 delays = np.zeros((n_obs_points, *grid_size))
 
 # Vectors r - r' for each grid point and each observation point
-r_diff = np.zeros((n_obs_points, 3, *grid_size))
+r_diff = np.zeros((n_obs_points, 2, *grid_size))
 
 for i, r in enumerate(r_obs):
-    for dim in range(3):
+    for dim in range(2):
         r_diff[i, dim] = r[dim] - grid_coordinates[dim]
     delays[i] = norm(r_diff[i], axis=0)/speed_of_light
 
@@ -64,7 +63,7 @@ for i, r in enumerate(r_obs):
     t_obs = np.linspace(t.min()+delays[i].max(), t.max()+delays[i].min(),
                         n_obs_times)
 
-    E_obs.append(np.zeros((n_obs_times, 3)))
+    E_obs.append(np.zeros((n_obs_times, 2)))
 
     # R is |r - r'| for each grid point
     R = delays[i] * speed_of_light
@@ -80,7 +79,6 @@ for i, r in enumerate(r_obs):
         rho_term = drho_dt(coords_tuple)
         Jx_term = dJx_dt(coords_tuple) / speed_of_light
         Jy_term = dJy_dt(coords_tuple) / speed_of_light
-        Jz_term = dJz_dt(coords_tuple) / speed_of_light
         a = 1
         b = 1
 
@@ -88,8 +86,6 @@ for i, r in enumerate(r_obs):
             np.sum(factor * (a * r_hat[0] * rho_term - b * Jx_term))
         E_obs[i][k, 1] = E_obs[i][k, 1] + dV * \
             np.sum(factor * (a * r_hat[1] * rho_term - b * Jy_term))
-        E_obs[i][k, 2] = E_obs[i][k, 2] + dV * \
-            np.sum(factor * (a * r_hat[2] * rho_term - b * Jz_term))
 
 
 fig, ax = plt.subplots(n_obs_points, sharex=True)
@@ -99,7 +95,6 @@ if not hasattr(ax, 'size'):
 for i, r in enumerate(r_obs):
     ax[i].plot(t_obs, E_obs[i][:, 0], label='Ex')
     ax[i].plot(t_obs, E_obs[i][:, 1], label='Ey')
-    ax[i].plot(t_obs, E_obs[i][:, 2], label='Ez')
     ax[i].plot(t_obs, norm(E_obs[i][:, :], axis=1), '--', label='||E||')
     ax[i].legend()
     ax[i].set_title(f'Observer {i+1} at {r}')
