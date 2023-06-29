ed56279a5a989f02dee1c470abfc0c77f40efe28
Jannis Teunissen <jannis@teunissen.net>
2023-06-29 15:34:05 +0200

    No longer need log file

--- simulation_data/combine_data.py
+++ simulation_data/combine_data.py
@@ -6,11 +6,10 @@ from scipy.constants import epsilon_0
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    description='TODO')
+    description='''Combine a series of npz files into a combined file for
+    the Jefimenko script''')
 parser.add_argument('npz', type=str, nargs='+',
-                    help='Npz files')
-parser.add_argument('-log_file', type=str, required=True,
-                    help='Afivo-streamer log file')
+                    help='Npz files containing rhs and Je terms')
 parser.add_argument('-output', type=str, default='combined_data.npz',
                     help='Output filename')
 args = parser.parse_args()
@@ -25,20 +24,18 @@ N = len(rhs_files)
 tmp = np.load(rhs_files[0])
 data_shape = tmp['uniform_data'].shape
 x, y, z = tmp['arr_0'], tmp['arr_1'], tmp['arr_2']
+t = np.zeros(N)
 
 rhs = np.zeros([N, *data_shape])
 J = np.zeros([N, 3, *data_shape])
 
 for i in range(N):
-    rhs[i] = np.load(rhs_files[i])['uniform_data']
-    rhs[i] = rhs[i] * -epsilon_0
+    rhs_data = np.load(rhs_files[i])
+    t[i] = rhs_data['time']
+    rhs[i] = rhs_data['uniform_data'] * -epsilon_0
     J[i, 0] = np.load(Je_1_files[i])['uniform_data']
     J[i, 1] = np.load(Je_2_files[i])['uniform_data']
     J[i, 2] = np.load(Je_3_files[i])['uniform_data']
-
-# Get time from the log file
-logfile = np.genfromtxt(args.log_file, skip_header=1)
-t = logfile[:N, 1]
 
 np.savez(args.output, rho=rhs, J=J, x=x, y=y, z=z, t=t)
 print(f'Written {args.output}')
