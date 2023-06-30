943873743b35072e5bb2382df345d30a16691f12
Jannis Teunissen <jannis@teunissen.net>
2023-06-30 13:44:44 +0200

    Add command line arguments to convert_data.sh

--- simulation_data/convert_data.sh
+++ simulation_data/convert_data.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-simulation_name=/export/scratch2/teunisse/tmp/hema_radio_3d
-silo_files=${simulation_name}_*.silo
+if [ $# -lt 2 ]; then
+   echo "Number of arguments should at least be 2."
+   echo "$0 resolution <all silo files>"
+   exit 1
+fi
+
+# Arguments: resolution <all silo files>
+resolution=$1
+shift
+silo_files="$@"
+
 script_dir=~/git/afivo/tools/
 vars="rhs Je_1 Je_2 Je_3"
-resolution=256
+r_min="0.06 0.06 0.05"
+r_max="0.14 0.14 0.2"
 
 (cd "$script_dir";
- parallel -j 6 ./plot_raw_data.py {1} -variable {2} -save_npz \
+ parallel -j 4 ./plot_raw_data.py {1} -variable {2} -save_npz \
           {1.}_uniform_${resolution}_{2}.npz \
           -min_pixels $resolution \
           -interpolation nearest \
-          -r_min 0.05 0.05 0.05 -r_max 0.15 0.15 0.15 \
+          -r_min ${r_min} -r_max ${r_max} \
           ::: $silo_files ::: $vars
- )
-
-# python combine_data.py ${simulation_name}_*_uniform_${resolution}*.npz \
-#        -log_file ${simulation_name}_log.txt -output test_${resolution}.npz
+)
