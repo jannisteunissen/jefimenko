9fe8ad619fa69f779bc8a70383248be8d1e93650
Hemaditya Malla <hemadity@solano.md.cwi.nl>
2023-09-13 11:09:13 +0200

    Also visualize static term

--- plot_jefimenko_results.py
+++ plot_jefimenko_results.py
@@ -10,7 +10,7 @@ from os.path import splitext
 
 p = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    description='Numerical solution to Jefimenko equations')
+    description='Visualize numerical solutions to Jefimenko equations')
 p.add_argument('data_files', type=str, nargs="+",
                help='''CSV files of different points to visualize''')
 p.add_argument('-filter_width', type=int, default=0,
@@ -30,7 +30,7 @@ def custom_filter(x):
 
 
 N = len(args.data_files)
-fig, ax = plt.subplots(N, 3, sharex='row', sharey=True, figsize=(8, N*3))
+fig, ax = plt.subplots(N, 4, sharex='row', sharey=True, figsize=(8, N*4))
 fig2, ax2 = plt.subplots(N, sharex=True, sharey=True)
 
 if N == 1:
@@ -48,47 +48,56 @@ for i, f in enumerate(args.data_files):
     data = np.loadtxt(f, delimiter=',', skiprows=1)
     t_obs = data[:, 0]
     t_src = data[:, 1]
-    E_obs_rho = data[:, 2:5]
-    E_obs_J = data[:, 5:]
-    ax[i, 0].plot(t_src, custom_filter(E_obs_rho[:, 0]), label='Ex')
-    ax[i, 0].plot(t_src, custom_filter(E_obs_rho[:, 1]), label='Ey')
-    ax[i, 0].plot(t_src, custom_filter(E_obs_rho[:, 2]), label='Ez')
-    tmp = savgol_filter(E_obs_rho[:, 2], 13, 1)
+    E_obs_space = data[:,2:5]
+    E_obs_rho = data[:, 5:8]
+    E_obs_J = data[:, 8:]
+    ax[i, 0].plot(t_src, custom_filter(E_obs_space[:, 0]), label='Ex')
+    ax[i, 0].plot(t_src, custom_filter(E_obs_space[:, 1]), label='Ey')
+    ax[i, 0].plot(t_src, custom_filter(E_obs_space[:, 2]), label='Ez')
+    # tmp = savgol_filter(E_obs_rho[:, 2], 13, 1)
     ax[i, 0].legend()
     ax[i, 0].set_ylabel('E (V/m)')
 
-    ax[i, 1].plot(t_src, custom_filter(E_obs_J[:, 0]), label='Ex')
-    ax[i, 1].plot(t_src, custom_filter(E_obs_J[:, 1]), label='Ey')
-    ax[i, 1].plot(t_src, custom_filter(E_obs_J[:, 2]), label='Ez')
+    ax[i, 1].plot(t_src, custom_filter(E_obs_rho[:, 0]), label='Ex')
+    ax[i, 1].plot(t_src, custom_filter(E_obs_rho[:, 1]), label='Ey')
+    ax[i, 1].plot(t_src, custom_filter(E_obs_rho[:, 2]), label='Ez')
 
-    ax[i, 2].plot(t_src, custom_filter(E_obs_rho[:, 0] + E_obs_J[:, 0]),
-                  label='Ex')
-    ax[i, 2].plot(t_src, custom_filter(E_obs_rho[:, 1] + E_obs_J[:, 1]),
-                  label='Ey')
-    ax[i, 2].plot(t_src, custom_filter(E_obs_rho[:, 2] + E_obs_J[:, 2]),
-                  label='Ez')
+    ax[i, 2].plot(t_src, custom_filter(E_obs_J[:, 0]), label='Ex')
+    ax[i, 2].plot(t_src, custom_filter(E_obs_J[:, 1]), label='Ey')
+    ax[i, 2].plot(t_src, custom_filter(E_obs_J[:, 2]), label='Ez')
 
-    ax[i, 2].text(1.1, 0.5, r'$r_\mathrm{obs}$ = ' + r,
+
+    tmp = custom_filter(E_obs_rho[:, 0] + E_obs_J[:, 0] + E_obs_space[:,0])
+    ax[i, 3].plot(t_src, tmp, label='Ex')
+    tmp = custom_filter(E_obs_rho[:, 1] + E_obs_J[:, 1] + E_obs_space[:,1])
+    ax[i, 3].plot(t_src, tmp, label='Ey')
+    tmp = custom_filter(E_obs_rho[:, 2] + E_obs_J[:, 2] + E_obs_space[:,2])
+    ax[i, 3].plot(t_src, tmp, label='Ez')
+
+
+    ax[i, 3].text(1.1, 0.5, r'$r_\mathrm{obs}$ = ' + r,
                   rotation='vertical',
                   horizontalalignment='center',
                   verticalalignment='center', transform=ax[i, 2].transAxes)
 
-    # Power spectrum
-    freq, Pxx_den = welch(custom_filter(E_obs_rho[:, 2]),
-                          1/(t_src[1] - t_src[0]))
+    # Power spectrum 
+    freq, Pxx_den = welch(custom_filter(norm(E_obs_rho + E_obs_J, axis=1)),
+                           1/(t_src[1] - t_src[0]), nperseg=32)
     ax2[i].semilogy(freq * 1e-6, Pxx_den)
     ax2[i].set_xlabel('Frequency (MHz)')
     ax2[i].set_ylabel('PSD [V**2/Hz]')
     ax2[i].set_title(f'Observer {i} at {r}')
 
 # Set labels only for certain plots
-ax[0, 0].set_title(r'$\partial_t \rho$ term')
-ax[0, 1].set_title(r'$\partial_t \vec{J}$ term')
-ax[0, 2].set_title(f'total')
+ax[0, 0].set_title(r'$\rho$ term')
+ax[0, 1].set_title(r'$\partial_t \rho$ term')
+ax[0, 2].set_title(r'$\partial_t \vec{J}$ term')
+ax[0, 3].set_title(f'total')
 
 ax[-1, 0].set_xlabel('t (s)')
 ax[-1, 1].set_xlabel('t (s)')
 ax[-1, 2].set_xlabel('t (s)')
+ax[-1, 3].set_xlabel('t (s)')
 
 #Saving the figures
 if args.save_figs.startswith("out"):
